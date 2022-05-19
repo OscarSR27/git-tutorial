69f85dd33d40ef6668c4b54861857d977df77d58
Alex Hoffman <alxhoff@gmail.com>
2022-05-20 00:22:04 +0200

    Apply patch to CMakeLists.txt and update source code according to requeriments of Coding Challenge II

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main()
 {
-    printf("Hello ESPL");
+    printf("Hello ESPL\n");
+            
+    char command = 'a';
+    char *numToWords = NULL;
+    while(command != 'q')
+    {
+        unsigned int x = 0;
+        printf("Please type an integer positive number (default value is 0): ");
+        scanf("%d", &x);
+
+        numToWords = num_to_words(x);
+
+        printf("The output value is: ");
+        printf("%s",numToWords);
+        printf("\nIf you want to exit please press q, otherwise press any other key\n");
+        scanf(" %c", &command);
+    }
+
 }
